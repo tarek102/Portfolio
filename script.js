a0fbce13048ced094091515aad9719d406e826dc
Tarek <83262890+tarek102@users.noreply.github.com>
2022-06-20 14:26:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,61 +89,54 @@ function goToAbout(e) {
 }
 
 // Events
+hamburger.addEventListener('click', openMenu);
+closeIcon.addEventListener('click', closeMenu);
+portfolioBtn.addEventListener('click', goToPortfolio);
+aboutBtn.addEventListener('click', goToAbout);
+contactBtn.addEventListener('click', goToContact);
+// dynamically create projects
 
+firstProjectDiv.innerHTML = `
+        <img src="./images/ImgPlaceholder.png" alt="image 1">
+        <div class="multi-post">
+          <h3>${firstProject.title}</h3>
+          <p>${firstProject.paragraph}</p>
+          <div class="skills">
+            <ul>
+              <li><span>${firstProject.skills[0]}</span></li>
+              <li><span>${firstProject.skills[1]}</span></li>
+              <li><span>${firstProject.skills[2]}</span></li>
+              <li><span>${firstProject.skills[3]}</span></li>
+            </ul>
+          </div>
+          <button class="btn first-btn">${firstProject.button}</button>
+        </div>`;
 
+projects.forEach((project) => {
+  const divElement = document.createElement('div');
+  mainProjectDiv.appendChild(divElement);
+  divElement.innerHTML = `
+  <div class="see-project-new">
+    <div class="project-new">
+      <h3>${project.title}</h3>
+      <p>${project.paragraph}</p>
+      <div class="skills-new">
+        <ul>
+          <li><span>${project.skills[0]}</span></li>
+          <li><span>${project.skills[1]}</span></li>
+          <li><span>${project.skills[2]}</span></li>
+        </ul>
+      </div>
+    </div>
+    <button class="projects-btn">${project.button}</button>
+  </div>`;
+});
 
 
 
 // On click create popup
 
-function popup(e) {
-  const popUpMainDiv = document.createElement('div');
-  const popupCard = document.createElement('div');
-  popupCard.className = 'popup-card';
-  popUpMainDiv.className = 'popup-main-div';
 
-  popupCard.innerHTML = `
-    <div class="popup-upper">
-      <h3>${projects[e].title}</h3>
-      <ul>
-        <li><span>${projects[e].skills[0]}</span></li>
-        <li><span>${projects[e].skills[1]}</span></li>
-        <li><span>${projects[e].skills[2]}</span></li>
-      </ul>
-      <i class="bi bi-x-lg popup-close-btn"></i>
-    </div>
-    <div class="popup-lower">
-      <div class="popup-lower-left">
-        <img src="./images/Snapshoot-Portfolio.png">
-      </div>
-
-      <div class="popup-lower-right">
-        <p>
-          Lorem Ipsum is simply dummy text of the
-          printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy
-          text ever since the 1500s, when an unknown printer took a
-          galley of type and scrambled it 1960s with the releaLorem
-          Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s
-          with the releax map lapora verita.
-        </p>
-        <div>
-          <button class="popup-lower-btn">See Live</button>
-          <button class="popup-lower-btn">See Source</button>
-        </div>
-
-      </div>
-    </div>
-    `;
-
-  mainProjectDiv.appendChild(popUpMainDiv);
-  popUpMainDiv.appendChild(popupCard);
-
-  const popupCloseBtn = document.querySelector('.popup-close-btn');
-  popupCloseBtn.addEventListener('click', () => {
-    mainProjectDiv.removeChild(popUpMainDiv);
-  });
-}
 
 // first project popup
 
