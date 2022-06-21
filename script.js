e24e851234d3c1cc05290ac8420ed233c7dc7b51
abass rashid <abazrashid144@gmail.com>
2022-06-21 11:00:06 +0300

    fix linter error

--- script.js
+++ script.js
@@ -271,14 +271,4 @@ const emailValue = document.getElementById('form-email');
 const errormessage = document.querySelector('.error');
 
 // submit event
-form.addEventListener('submit', (event) => {
-  let errorMsg = '';
-  const email = emailValue.value.toLowerCase();
-  if (email !== emailValue.value) {
-    errorMsg = 'Your form cannot be submitted, please turn it to lowercase and resubmit';
-  }
-  if (errorMsg.length > 0) {
-    event.preventDefault();
-    errormessage.innerHTML = errorMsg;
-  }
-});
+
